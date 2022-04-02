815084961a9e106aca4a6cf3e9a40947a6d8aa70
BurritoSOFTWARE <37681398+burritosoftware@users.noreply.github.com>
2022-04-02 11:05:30 -0700

    Update bemani.user.js

--- bemani.user.js
+++ bemani.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://cdn.discordapp.com/attachments/558842854462717954/959871251696992266/dotted-place-bemani.png";
+                i.src = "https://website.burrito.software/build.png";
                 i.onload = () => {
                     if (i.width === i.height) {
                         i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
